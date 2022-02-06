4ba663682e5811d815d35f457751f1f1eb1d9b5e
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-06 18:30:52 +0300

    Добавление проекта WebHost

---
+++ WebAPIApp.WebHost/Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIApp.Core.Abstractions.Repositories;
+using WebAPIApp.Core.Domain.Administration;
+using WebAPIApp.DataAccess.Data;
+using WebAPIApp.DataAccess.Repositories;
+
+namespace WebAPIApp.WebHost
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddScoped(typeof(IRepository<Employee>), (x) =>
+                new InMemoryRepository<Employee>(FakeDataFactory.Employees));
+            services.AddScoped(typeof(IRepository<Role>), (x) =>
+                new InMemoryRepository<Role>(FakeDataFactory.Roles));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIApp.WebHost", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIApp.WebHost v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
