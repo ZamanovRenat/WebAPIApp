8844f260170996857f5dd791b7eb7d037996cc73
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-06 17:49:44 +0300

    Добавление первых данных по ролям и пользователям

---
+++ WebAPIApp.DataAccess/Repositories/InMemoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIApp.Core.Abstractions.Repositories;
+using WebAPIApp.Core.Domain;
+
+namespace WebAPIApp.DataAccess.Repositories
+{
+    public class InMemoryRepository<T>
+        : IRepository<T>
+        where T : BaseEntity
+    {
+        protected IEnumerable<T> Data { get; set; }
+
+        public InMemoryRepository(IEnumerable<T> data)
+        {
+            Data = data;
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            return Task.FromResult(Data);
+        }
+
+        public Task<T> GetByIdAsync(Guid id)
+        {
+            return Task.FromResult(Data.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
