b9b1281dd81d330c12f5e0381eb09fab2d21654f
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-06 17:28:18 +0300

    Добавление Swaqqer в проект

--- WebAPIApp/Startup.cs
+++ WebAPIApp/Startup.cs
@@ -16,6 +16,9 @@ namespace WebAPIApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +30,14 @@ namespace WebAPIApp
             }
 
             app.UseRouting();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3(x =>
+            {
+                x.DocExpansion = "list";
+            });
+
+            app.UseHttpsRedirection();
 
             app.UseEndpoints(endpoints =>
             {
