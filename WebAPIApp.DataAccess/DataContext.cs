45437385795cb13af7607d5c53c885ba5bf9e88c
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-07 20:02:52 +0300

    Добавление класса контекста базы данных

---
+++ WebAPIApp.DataAccess/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIApp.Core.Domain.Administration;
+
+namespace WebAPIApp.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> dbContextOptions) 
+            : base(dbContextOptions)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
