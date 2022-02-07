d6120bd877ae7917c33bba595aae1484152ed101
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-07 15:03:37 +0300

    Добавление вспомогательной модели для создания и редактирования сотрудников

---
+++ WebAPIApp.WebHost/Models/CreateOrEditEmployeeRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPIApp.WebHost.Models
+{
+    public class CreateOrEditEmployeeRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string[] RoleNames { get; set; }
+        public int AppliedPromocodesCount { get; set; }
+    }
+}
