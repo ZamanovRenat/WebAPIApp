dea5faf12d1069ba25b9dbfdd3caab7d9ccb4be4
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-07 15:06:59 +0300

    Создание отдельных классов мапперов, которые будут отвечать за маппинг моделей и DTO

---
+++ WebAPIApp.WebHost/Mappers/EmployeeStaticMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using WebAPIApp.Core.Domain.Administration;
+using WebAPIApp.WebHost.Models;
+
+namespace WebAPIApp.WebHost.Mappers
+{
+    public class EmployeeStaticMapper
+    {
+        public static Employee MapFromModel(CreateOrEditEmployeeRequest model, List<Role> roles,
+            Employee employee = null)
+        {
+            if (employee == null)
+            {
+                employee = new Employee();
+                employee.Id = Guid.NewGuid();
+            }
+
+            employee.FirstName = model.FirstName;
+            employee.LastName = model.LastName;
+            employee.AppliedPromocodesCount = model.AppliedPromocodesCount;
+            employee.Email = model.Email;
+            employee.Roles = roles;
+
+            return employee;
+        }
+    }
+}
---
+++ WebAPIApp.WebHost/Mappers/IEmployeeMapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using WebAPIApp.Core.Domain.Administration;
+using WebAPIApp.WebHost.Models;
+
+namespace WebAPIApp.WebHost.Mappers
+{
+    public interface IEmployeeMapper
+    {
+        Task<Employee> MapFromModelAsync(CreateOrEditEmployeeRequest model,
+            Employee employee = null);
+    }
+}
