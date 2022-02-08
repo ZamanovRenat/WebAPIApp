42caba99882b15609b398e82f9abac0db4e18713
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-08 17:32:53 +0300

    Добавление класса репозитория базы данных

---
+++ WebAPIApp.DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIApp.Core.Abstractions.Repositories;
+using WebAPIApp.Core.Domain;
+
+namespace WebAPIApp.DataAccess.Repositories
+{
+    public class EfRepository<T>
+        : IRepository<T>
+        where T : BaseEntity
+    {
+        private readonly DataContext _dataContext;
+
+        public EfRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var entities = await _dataContext.Set<T>().ToListAsync();
+
+            return entities;
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            var entity = await _dataContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+
+            return entity;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _dataContext.Set<T>().AddAsync(entity);
+
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dataContext.Set<T>().Remove(entity);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public Task<IEnumerable<T>> GetByCondition(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
